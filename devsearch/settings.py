f31822f2ac34272a3528545db402fcc8b10734e0
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-07-24 18:40:25 +0400

    added styles

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nq97eg4y0j#g=$d7y^gxsj)5nzlbr^r)n0f@)5l!5xdbfbb-qt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
