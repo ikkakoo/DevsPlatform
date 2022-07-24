1ca75cd02831cbba0604dff3ab57ff9282d7eb7d
ikkakoo <dzvelaia.irakli1@gmail.com>
2022-07-24 13:31:50 +0400

    added update functionality

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-from projects.views import projetcts, project, create_project
+from projects.views import projetcts, project, create_project, update_project
 
 
 urlpatterns = [
     path('', projetcts, name='projects'),
     path('project/<str:pk>/', project, name='project'),
-    path('create-project/', create_project, name='create-project')
+    path('create-project/', create_project, name='create-project'),
+    path('update-project/<str:pk>/', update_project, name='update-project'),
 ]
